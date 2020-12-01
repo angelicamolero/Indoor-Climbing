54184d838cb585e6a8c8e0bc91eda2cc4d2a8991
angelicamolero <angelicamoleror8@gmail.com>
2020-11-30 21:56:49 -0300

    adding BEM

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,16 +6,16 @@ import Button  from './Button';
 const Header = () => {
     return(
         <header className="header">
-            <div className="logo-box">
-               <img src={logo} alt="Logo" className="logo"/>
+            <div className="header__logo-box">
+               <img src={logo} alt="Logo" className="header__logo"/>
             </div>
-            <div className="text-box">
+            <div className="header__text-box">
             <h1 className="heading-primary">
-                <span className="heading-primary-main">Indoors</span>
-                <span className="heading-primary-sub">get prepare for the outside</span>
+                <span className="heading-primary--main">Indoors</span>
+                <span className="heading-primary--sub">get prepare for the outside</span>
             </h1>
             <Button
-             css="btn-white btn-animated"
+             css="btn--white btn--animated"
              text="Discover indoor climbing"
             />
             </div>
