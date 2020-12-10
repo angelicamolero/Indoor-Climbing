3882bb62db28e2feca6cb8528175cfe1075b1b38
angelicamolero <angelicamoleror8@gmail.com>
2020-12-10 13:21:26 -0300

    changing to large

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,9 @@ import Button  from './utils/Button';
 import HeadingSecondary from './utils/HeadingSecondary';
 import HeadingTertiary from './utils/HeadingTertiary';
 import Paragraph from './utils/Paragraph';
+import Photo1 from "../img/nat-1-large.jpg";
+import Photo2 from "../img/nat-2-large.jpg";
+import Photo3 from "../img/nat-3-large.jpg";
 
 const About = () => {
     return(
@@ -39,9 +42,9 @@ const About = () => {
                     </div>
                     <div class="col-1-of-2">
                     <div className="composition">
-                        <img src="../img/nat-1.jpg" alt="Photo 1" className="composition__photo composition__photo--p1"/>
-                        <img src="../img/nat-2.jpg" alt="Photo 2" className="composition__photo composition__photo--p2"/>
-                        <img src="../img/nat-3.jpg" alt="Photo 3" className="composition__photo composition__photo--p3"/>
+                        <img src={Photo1} alt="Photo 1" className="composition__photo composition__photo--p1"/>
+                        <img src={Photo2} alt="Photo 2" className="composition__photo composition__photo--p2"/>
+                        <img src={Photo3} alt="Photo 3" className="composition__photo composition__photo--p3"/>
                     </div>
                     </div>
                 </div>
