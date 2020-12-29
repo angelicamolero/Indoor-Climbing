08b8b4d793fc898f4d06e0509f175065a3b30bee
angelicamolero <angelicamoleror8@gmail.com>
2020-12-28 21:29:17 -0300

    adding blend-mode to img card

--- src/components/utils/CardTour.js
+++ src/components/utils/CardTour.js
@@ -11,7 +11,15 @@ const CardTour = ({tourCard}) => {
             <div className="col-1-of-3">
                 <div className="tour-card">
                     <div className="tour-card__side tour-card__side--front">
-                        front
+                    <div className={`tour-card__picture tour-card__picture-${tourCard.id}`}>
+                    &nbsp;
+                    </div>
+                    <div className="tour-card__heading">
+                    Heading
+                    </div>
+                    <div className="tour-card__details">
+                    DETAILS
+                    </div>
                     </div>
                     <div className={`tour-card__side tour-card__side--back tour-card__side--back-${tourCard.id}`}>
                         back
