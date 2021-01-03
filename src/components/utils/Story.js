4d4d56193f15c390cfb65c9f682b7361f2c54b7a
angelicamolero <angelicamoleror8@gmail.com>
2021-01-03 13:07:56 -0300

    adding circle img

--- src/components/utils/Story.js
+++ src/components/utils/Story.js
@@ -3,13 +3,13 @@ import HeadingTertiary from './HeadingTertiary';
 import Paragraph from './Paragraph';
 
 
-
 const Story = ({story}) => {
+    const image = require(`../../img/${story.img}`);
     
     return (
         <div className="story">
             <figure className="story__shape">
-                 
+                 <img src={image.default} className="story__img" alt="person on a tour"/>
             </figure>
             <div className="story__text">
                 <HeadingTertiary
